da618f358ee39a91fe91a5246e977f5b150b7696
freshtaste <liujizhou123@hotmail.com>
2022-04-20 22:26:52 -0500

    add sim3

--- multiple_factor.py
+++ multiple_factor.py
@@ -7,7 +7,7 @@ from nbpmatching import match_tuple
 
 class DGP2(object):
     
-    def __init__(self, num_factor, num_sample, Xdim, tau=0, match_more=False):
+    def __init__(self, num_factor, num_sample, Xdim, tau=0, match_more=False, design='MT'):
         self.tuple_size = 2**num_factor
         self.num_factor = num_factor
         if num_sample%(self.tuple_size*2) == 0:
@@ -17,9 +17,13 @@ class DGP2(object):
         self.tau = tau
         self.Xdim = Xdim
         self.match_more = match_more
+        self.design = design
+        if match_more:
+            if self.design != 'MT':
+                raise ValueError("match_more is true only under MT")
+        self.tuple_idx = None
         self.all_treatments = self.get_treatment_combination()
         self.X = self.generate_X()
-        self.tuple_idx = self.get_tuple_idx()
         self.D = self.generate_D()
         self.Y = self.generate_Y()
         if self.match_more:
@@ -30,11 +34,32 @@ class DGP2(object):
         return X
     
     def generate_D(self):
-        df = pd.DataFrame(self.tuple_idx)
-        idx = df.apply(lambda x:np.random.shuffle(x) or x, axis=1).to_numpy()
-        D = np.zeros((self.n, self.num_factor))
-        for c in range(idx.shape[1]):
-            D[idx[:,c]] = np.array([np.array(self.all_treatments[c])]*int(self.n/len(self.all_treatments)))
+        if self.design == 'MT':
+            self.tuple_idx = self.get_tuple_idx()
+            df = pd.DataFrame(self.tuple_idx)
+            idx = df.apply(lambda x:np.random.shuffle(x) or x, axis=1).to_numpy()
+            D = np.zeros((self.n, self.num_factor))
+            for c in range(idx.shape[1]):
+                D[idx[:,c]] = np.array([np.array(self.all_treatments[c])]*int(self.n/len(self.all_treatments)))
+        elif self.design == 'C':
+            D = np.array(self.all_treatments*int(self.n/len(self.all_treatments)))
+        elif self.design == 'S4':
+            self.tuple_idx = np.zeros(self.n)
+            D = np.zeros((self.n, self.num_factor))
+            X = (self.X - .5).dot(np.linspace(1,2,self.Xdim))
+            idx_s1, idx_s2 = X <= np.quantile(X, .25), (X <= np.median(X)) & (np.quantile(X, .25) < X)
+            idx_s3, idx_s4 = (X <= np.quantile(X, .75)) & (np.median(X) < X), np.quantile(X, .75) < X
+            D[idx_s1] = np.array(self.all_treatments*int(self.n/len(self.all_treatments)/4))
+            D[idx_s2] = np.array(self.all_treatments*int(self.n/len(self.all_treatments)/4))
+            D[idx_s3] = np.array(self.all_treatments*int(self.n/len(self.all_treatments)/4))
+            D[idx_s4] = np.array(self.all_treatments*int(self.n/len(self.all_treatments)/4))
+            self.tuple_idx[idx_s2] = 1
+            self.tuple_idx[idx_s3] = 2
+            self.tuple_idx[idx_s4] = 3
+        elif self.design == 'RE':
+            pass
+        else:
+            raise ValueError("Design is not valid.")
         return D
     
     def generate_Y(self):
@@ -64,14 +89,16 @@ class DGP2(object):
 
 class Inferece2(object):
     
-    def __init__(self, Y, D, tuple_idx):
+    def __init__(self, Y, D, tuple_idx, design='MT'):
         self.Y = Y
         self.D = D
         self.n, self.tuple_size = self.D.shape[0], 2**(self.D.shape[1])
-        self.num_factor = log2(self.tuple_size)
+        self.num_factor = int(log2(self.tuple_size))
         self.tuple_idx = tuple_idx
+        self.design = design
+        self.phi_tau, self.phi_tau_p = None, None
         self.tau = self.estimator()
-        self.phi_tau, self.phi_tau_p = self.inference()
+        self.inference()
         
     def estimator(self):
         Y, D = self.Y, self.D
@@ -105,8 +132,41 @@ class Inferece2(object):
         phi_tau = 1 if np.abs(self.tau)/np.sqrt(var_tau/(n/d)) > 1.96 else 0
         return phi_tau
     
-    def inference(self):
+    def inference_MT(self):
         return self.get_reject(rho_type='classic'), self.get_reject(rho_type='pairs-on-pairs')
+    
+    def inference(self):
+        all_treatments = list(itertools.product([0, 1], repeat=self.num_factor))
+        v = self.get_select_vector()
+        if self.design == 'MT':
+            self.phi_tau, self.phi_tau_p = self.inference_MT()
+        elif self.design == 'C':
+            V = np.diag([np.var(self.Y[np.prod(self.D==d,axis=1)==1]) for d in all_treatments])
+            # compute variance
+            var_tau = v.dot(V).dot(v)
+        elif self.design == 'S4':
+            num_strata = len(set(self.tuple_idx))
+            num_treatment = len(all_treatments)
+            sigma = np.zeros((num_strata,num_treatment))
+            mu = np.zeros((num_strata,num_treatment))
+            for i, d in enumerate(all_treatments):
+                for s in range(num_strata):
+                    Y_ds = self.Y[(np.prod(self.D==d,axis=1)==1) & (self.tuple_idx==s)]
+                    #sigma[s, d] = np.var(Y_ds)
+                    mu[s, i] = np.mean(Y_ds)
+            #V1 = np.diag(np.mean(sigma, axis=0))
+            #V2 = np.cov(mu.T)/num_treatment
+            #V = V1 + V2
+            Ybar = np.mean(mu, axis=0)
+            Y2 = np.array([np.mean(self.Y[np.prod(self.D==d,axis=1)==1]**2) for d in all_treatments])
+            sigma2 = Y2 - np.mean(mu**2, axis=0)
+            var_tau = v.dot(np.diag(sigma2)).dot(v) + np.mean((mu - Ybar).dot(v)**2)/num_treatment
+            #print(self.tau,var_tau,sigma2,v.dot(np.diag(sigma2)).dot(v), Y2,np.mean(mu**2, axis=0))
+        else:
+            raise ValueError("Design is not valid.")
+        
+        self.phi_tau = 1 if np.abs(self.tau)/np.sqrt(var_tau/(self.n/self.tuple_size)) > 1.96 else 0
+        return None
     
     def get_select_vector(self):
         v = np.zeros(self.tuple_size)
