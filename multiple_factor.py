f6d071a429911c460bd585aca0ab9c46cd19f19f
freshtaste <liujizhou123@hotmail.com>
2022-04-29 15:30:25 -0500

    modified sim3

--- multiple_factor.py
+++ multiple_factor.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from math import log2
+from scipy.stats import chi2
 import itertools
 from nbpmatching import match_tuple
 
@@ -22,6 +23,7 @@ class DGP2(object):
             if self.design != 'MT':
                 raise ValueError("match_more is true only under MT")
         self.tuple_idx = None
+        self.Xall = None
         self.all_treatments = self.get_treatment_combination()
         self.X = self.generate_X()
         self.D = self.generate_D()
@@ -30,7 +32,8 @@ class DGP2(object):
             self.tuple_idx = self.tuple_idx.reshape(-1,self.tuple_size)
         
     def generate_X(self):
-        X = np.random.uniform(0,1,size=(self.n,self.Xdim))
+        self.Xall = np.random.uniform(0,1,size=(self.n,10))
+        X = self.Xall[:,:self.Xdim]
         return X
     
     def generate_D(self):
@@ -47,7 +50,7 @@ class DGP2(object):
             self.tuple_idx = np.zeros(self.n)
             D = np.zeros((self.n, self.num_factor))
             #X = (self.X - .5).dot(np.linspace(1,2,self.Xdim))
-            X = self.X[:,-1]
+            X = self.X[:,0]
             idx_s1, idx_s2 = X <= np.quantile(X, .25), (X <= np.median(X)) & (np.quantile(X, .25) < X)
             idx_s3, idx_s4 = (X <= np.quantile(X, .75)) & (np.median(X) < X), np.quantile(X, .75) < X
             D[idx_s1] = np.array(self.all_treatments*int(self.n/len(self.all_treatments)/4))
@@ -58,15 +61,19 @@ class DGP2(object):
             self.tuple_idx[idx_s3] = 2
             self.tuple_idx[idx_s4] = 3
         elif self.design == 'RE':
-            eps = 2e-5*(self.Xdim**.5)*np.sqrt(self.num_factor*2)
-            dist = 100
-            while dist > eps:
-                D = np.array(self.all_treatments*int(self.n/len(self.all_treatments)))
+            a = chi2.ppf(.01**(1/self.num_factor), self.Xdim)
+            Mf_max = 100
+            D = np.array(self.all_treatments*int(self.n/len(self.all_treatments)))
+            while Mf_max > a:
                 idx = np.random.permutation(self.n)
                 D = D[idx]
-                taux = np.array([np.mean(self.X[D[:,f]==1] - self.X[D[:,f]==0], axis=0) for f in range(self.num_factor)])
-                dist = np.mean(taux**2)
-                #print(dist)
+                #taux = np.array([np.mean(self.X[D[:,f]==1] - self.X[D[:,f]==0], axis=0) for f in range(self.num_factor)])
+                Mf_max = 0
+                for f in range(self.num_factor):
+                    x_diff = np.mean(self.X[D[:,f]==1] - self.X[D[:,f]==0], axis=0)
+                    Mf = x_diff.dot(x_diff)*12*self.n/4
+                    if Mf > Mf_max:
+                        Mf_max = Mf
         elif self.design == 'MP-B':
             self.tuple_idx = match_tuple(self.X, 1)
             df = pd.DataFrame(self.tuple_idx)
@@ -79,14 +86,16 @@ class DGP2(object):
         return D
     
     def generate_Y(self):
-        n, X, D = self.n, self.X, self.D
+        n, X, D = self.n, self.Xall, self.D
         eps = np.random.normal(0, 0.1, size=n)
         if D.shape[1] > 1:
             gamma = 2*D[:,1] - 1
+            Y = gamma*(X - .5).dot(np.linspace(2,1,10)) \
+                + (np.mean(D[:,1:],axis=1) + D[:,0])*self.tau + eps
         else:
             gamma = 1
-        Y = gamma*(X - .5).dot(np.linspace(1,2,self.Xdim)) \
-            + (np.sum(D[:,1:],axis=1)/self.num_factor + D[:,0])*self.tau + eps
+            Y = gamma*(X - .5).dot(np.linspace(2,1,10)) \
+                + D[:,0]*self.tau + eps
         return Y
         
     def get_treatment_combination(self):
@@ -164,6 +173,7 @@ class Inferece2(object):
             V = np.diag([np.var(self.Y[np.prod(self.D==d,axis=1)==1]) for d in all_treatments])
             # compute variance
             var_tau = v.dot(V).dot(v)
+            self.phi_tau = 1 if np.abs(self.tau)/np.sqrt(var_tau/(self.n/self.tuple_size)) > 1.96 else 0
         elif self.design == 'S4':
             num_strata = len(set(self.tuple_idx))
             num_treatment = len(all_treatments)
@@ -182,10 +192,10 @@ class Inferece2(object):
             sigma2 = Y2 - np.mean(mu**2, axis=0)
             var_tau = v.dot(np.diag(sigma2)).dot(v) + np.mean((mu - Ybar).dot(v)**2)/num_treatment
             #print(self.tau,var_tau,sigma2,v.dot(np.diag(sigma2)).dot(v), Y2,np.mean(mu**2, axis=0))
+            self.phi_tau = 1 if np.abs(self.tau)/np.sqrt(var_tau/(self.n/self.tuple_size)) > 1.96 else 0
         else:
             raise ValueError("Design is not valid.")
         
-        self.phi_tau = 1 if np.abs(self.tau)/np.sqrt(var_tau/(self.n/self.tuple_size)) > 1.96 else 0
         return None
     
     def get_select_vector(self):
